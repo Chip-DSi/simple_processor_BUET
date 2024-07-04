546a4da015b0fb4fdeaacc56f8b15df3fc6dfb1b
Bokhtiar Foysol Himon <bokhtiarfoysol@gmail.com>
2024-07-04 11:06:30 +0600

    changed formatting

--- rtl/alu_shift.sv
+++ rtl/alu_shift.sv
@@ -11,26 +11,26 @@ import simple_processor_pkg::DATA_WIDTH;
     parameter int SHIFT_WIDTH = 5
 ) (
     //-PORTS
-    input logic   [DATA_WIDTH - 1:0]  rs1_data_i,
-    input logic   [DATA_WIDTH - 1:0]  rs2_data_i,
-    input logic   [DATA_WIDTH - 1:0]  func_t,
+    input logic   [DATA_WIDTH - 1:0 ]   rs1_data_i, //input data from Rs1
+    input logic   [DATA_WIDTH - 1:0 ]   rs2_data_i, //input data from Rs2
+    input logic   [15:0 ]               func_t,     //input func_t from Instruction Decoder
 
-    output logic [DATA_WIDTH - 1:0] result
+    output logic  [DATA_WIDTH - 1:0 ]   result      //output result
 );
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-LOCALPARAMS GENERATED
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  logic                     shift_r;
-  logic [DATA_WIDTH - 1:0]  imm;
-  logic [DATA_WIDTH-1:0]    imm_extended;
-  logic [SHIFT_WIDTH- 1:0] 
-  logic [SHIFT_WIDTH - 1:0]  shift_amount;
-  logic [DATA_WIDTH-1:0]    stage[SHIFT_WIDTH];
-  logic [DATA_WIDTH-1:0]    lr_init;
-  logic [DATA_WIDTH-1:0]    lr_final;
-
+  logic                                shift_r;           //shift right if HIGH, shift left if LOW
+  logic           [DATA_WIDTH - 1:0 ]  imm;               //extracted imm from func_t
+  logic           [DATA_WIDTH - 1:0 ]  imm_extended;      //extended 32 bit imm
+  logic           [SHIFT_WIDTH - 1:0]  shift_amount;      //number of bits we want to shift
+                                                          //extracted from imm or Rs2
+  logic           [DATA_WIDTH - 1:0 ]  stage[SHIFT_WIDTH];//array of registers representing 
+                                                          //intermediate stages
+  logic           [DATA_WIDTH - 1:0 ]  lr_init;
+  logic           [DATA_WIDTH - 1:0 ]  lr_final;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-TYPEDEFS
@@ -43,8 +43,9 @@ import simple_processor_pkg::DATA_WIDTH;
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  assign imm = [9:4]func_t;  
-  assign imm_extended = {{26{imm[5]}}, imm};
+  
+  assign imm = func_t[9:4];  //extracting immediate from func_t
+  assign imm_extended = {{26{imm[5]}}, imm};//extending immediate
 
   always_comb begin
     case(func_t)
@@ -57,27 +58,26 @@ import simple_processor_pkg::DATA_WIDTH;
       SLRI    : shift_r = '1,
                 shift_amount = imm_extended;
       default : shift_r = '0,
-                shift_amount = rs2_data_i;;
+                shift_amount = rs2_data_i;
     endcase
   end
 
-  for (genvar i = 0; i < DATA_WIDTH; i++) begin : g_right_shift_invertions
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  //-RTLS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  for (genvar i = 0; i < DATA_WIDTH; i++) begin
     assign lr_init[i] = shift_r ? rs1_data_i[DATA_WIDTH-1-i] : rs1_data_i[i];
     assign lr_final[i] = shift_r ? stage[SHIFT_WIDTH-1][DATA_WIDTH-1-i]
                                        : stage[SHIFT_WIDTH-1][i];
   end
 
   assign stage[0] = shift_amount[0] ? {lr_init, 1'b0}: lr_init;
-  for (genvar i = 1; i < SHIFT_WIDTH; i++) begin : g_shift_mux
+  for (genvar i = 1; i < SHIFT_WIDTH; i++) begin
     assign stage[i] = shift_amount[i] ? {stage[i-1], {(2**i){1'b0}}} : stage[i-1];
   end
 
   assign result = lr_final;
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-RTLS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-METHODS
