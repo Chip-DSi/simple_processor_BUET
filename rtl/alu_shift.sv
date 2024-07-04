0230e4ff645645c4ba3088ccadea8082ca9ed1a9
Bokhtiar Foysol Himon <bokhtiarfoysol@gmail.com>
2024-07-04 12:12:54 +0600

    added alu_shift

--- rtl/alu_shift.sv
+++ rtl/alu_shift.sv
@@ -1,56 +1,14 @@
+
 /*
 Write a markdown documentation for this systemverilog module:
 Author : Bokhtiar Foysol Himon (bokhtiarfoysol@gmail.com)
 */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 08e129931861f2d50972c98ad04a93f388f2c99e
 module alu_shift 
 import simple_processor_pkg::DATA_WIDTH;
 #(
     //-PARAMETERS
     //-LOCALPARAMS
-<<<<<<< HEAD
-    parameter int DATA_WIDTH = 32
-) (
-    //-PORTS
-    input logic   [DATA_WIDTH - 1:0] rs1_data_i,
-    input logic   [DATA_WIDTH - 1:0] rs2_data_i,
-    input instr_t func_i,
-
-    output logic [DATA_WIDTH - 1:0] result
-=======
-module shift_left (
-    input  logic        clk,
-    input  logic [31:0] data_in,
-    input  logic [4:0]  shift_amount,  // assuming 5-bit shift amount to cover shift ranges from 0 to 31
-    output logic [31:0] data_out
-);
-
-    always_ff @(posedge clk) begin
-        data_out <= data_in << shift_amount;
-    end
-
-endmodule
-
-module alu_shift #(
-    //-PARAMETERS
-    //-LOCALPARAMS
-    parameter int DATA_WIDTH = 32
-) (
-    //-PORTS
-    input logic clk,
-    input logic [DATA_WIDTH - 1:0] rs1_i,
-    input logic [DATA_WIDTH - 1:0] rs2_i,
-    input logic [DATA_WIDTH - 1:0] imm,
-    input logic use_imm,
-    input logic shift_l
-
-    output logic [DATA_WIDTH - 1:0] rd_o
->>>>>>> 33525d2 (added SLL, SLLI, SLR, SLRI to alu_shift)
-=======
     parameter int SHIFT_WIDTH = 5
 ) (
     //-PORTS
@@ -59,28 +17,12 @@ module alu_shift #(
     input logic   [15:0 ]               func_t,     //input func_t from Instruction Decoder
 
     output logic  [DATA_WIDTH - 1:0 ]   result      //output result
->>>>>>> 08e129931861f2d50972c98ad04a93f388f2c99e
 );
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-LOCALPARAMS GENERATED
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  logic                     shift_r;
-  logic                     use_imm;
-  logic [DATA_WIDTH - 1:0]  imm;
-  logic [SHIFT_WIDTH- 1:0] 
-  logic [SHIFT_WIDTH - 1:0]  shift_amount;
-  logic [DATA_WIDTH-1:0]    stage[SHIFT_WIDTH];
-  logic [DATA_WIDTH-1:0]    lr_init;
-  logic [DATA_WIDTH-1:0]    lr_final;
-
-=======
-logic [DATA_WIDTH - 1:0] shift_amount;
->>>>>>> 33525d2 (added SLL, SLLI, SLR, SLRI to alu_shift)
-=======
   logic                                shift_r;           //shift right if HIGH, shift left if LOW
   logic           [DATA_WIDTH - 1:0 ]  imm;               //extracted imm from func_t
   logic           [DATA_WIDTH - 1:0 ]  imm_extended;      //extended 32 bit imm
@@ -90,7 +32,6 @@ logic [DATA_WIDTH - 1:0] shift_amount;
                                                           //intermediate stages
   logic           [DATA_WIDTH - 1:0 ]  lr_init;
   logic           [DATA_WIDTH - 1:0 ]  lr_final;
->>>>>>> 08e129931861f2d50972c98ad04a93f388f2c99e
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-TYPEDEFS
@@ -103,19 +44,6 @@ logic [DATA_WIDTH - 1:0] shift_amount;
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
-<<<<<<< HEAD
-always_comb begin
-    if(use_imm)
-        shift_amount = imm;
-    else
-        shift_amount = rs2_i;
-end
-
-<<<<<<< HEAD
-
-
-  for (genvar i = 0; i < DATA_WIDTH; i++) begin : g_right_shift_invertions
-=======
   
   assign imm = func_t[9:4];  //extracting immediate from func_t
   assign imm_extended = {{26{imm[5]}}, imm};//extending immediate
@@ -140,38 +68,17 @@ end
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
   for (genvar i = 0; i < DATA_WIDTH; i++) begin
->>>>>>> 08e129931861f2d50972c98ad04a93f388f2c99e
     assign lr_init[i] = shift_r ? rs1_data_i[DATA_WIDTH-1-i] : rs1_data_i[i];
     assign lr_final[i] = shift_r ? stage[SHIFT_WIDTH-1][DATA_WIDTH-1-i]
                                        : stage[SHIFT_WIDTH-1][i];
   end
 
   assign stage[0] = shift_amount[0] ? {lr_init, 1'b0}: lr_init;
-<<<<<<< HEAD
-  for (genvar i = 1; i < SHIFT_WIDTH; i++) begin : g_shift_mux
-    assign stage[i] = shift_amount[i] ? {stage[i-1], {(2**i){1'b0}}} : stage[i-1];
-  end
-
-  assign data_o = lr_final;
-=======
->>>>>>> 33525d2 (added SLL, SLLI, SLR, SLRI to alu_shift)
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-RTLS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-always_ff @(posedge clk) begin
-    if(shift_l)
-        rd_o <= rs1_i << shift_amount;
-    else
-        rd_o <= rs1_i >> shift_amount;
-end
-=======
   for (genvar i = 1; i < SHIFT_WIDTH; i++) begin
     assign stage[i] = shift_amount[i] ? {stage[i-1], {(2**i){1'b0}}} : stage[i-1];
   end
 
   assign result = lr_final;
->>>>>>> 08e129931861f2d50972c98ad04a93f388f2c99e
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-METHODS
