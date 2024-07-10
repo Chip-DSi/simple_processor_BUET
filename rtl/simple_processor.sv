c2b9c01dbe7aa81304d23b3978da86162321a654
Mymuna Khatun Sadia <maimuna14400@gmail.com>
2024-07-11 02:22:29 +0600

    id and execution block

--- rtl/simple_processor.sv
+++ rtl/simple_processor.sv
@@ -4,16 +4,16 @@ Author : name (email)
 */
 
 module simple_processor #(
-    parameter int MEM_ADDR_WIDTH = simple_processor_pkg::ADDR_WIDTH,  // With of memory address bus
-    parameter int MEM_DATA_WIDTH = simple_processor_pkg::DATA_WIDTH   // With of memory data bus
+    parameter int MEM_ADDR_WIDTH = simple_processor_pkg::ADDR_WIDTH,  // Width of memory address bus
+    parameter int MEM_DATA_WIDTH = simple_processor_pkg::DATA_WIDTH   // Width of memory data bus
 ) (
     // Global Synchronous Clock
-    input logic clk_i,
+    input logic                       clk_i,
     // Active low asynchronous reset
-    input logic arst_ni,
+    input logic                       arst_ni,
 
     // Boot address of the processor
-    input logic [MEM_ADDR_WIDTH-1:0] boot_addr_i,
+    input logic [MEM_ADDR_WIDTH-1:0]  boot_addr_i,
 
     // Signifies there is active request for memory at address imem_addr_o
     output logic                      imem_req_o,
@@ -39,50 +39,69 @@ module simple_processor #(
 );
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-LOCALPARAMS GENERATED
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-TYPEDEFS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
   //-SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  logic [MEM_ADDR_WIDTH-1:0] imm_pc_i;// intermediate result
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-ASSIGNMENTS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-  // always_comb begin
-  //   case(valid_pc)
-  //     1       :  imm_pc_i   = imem_addr_o + 2; // next pc
-  //     0       :  imm_pc_i   = boot_addr_i;     // boot address
-  //     default :  imm_pc_i   = boot_addr_i;     // for default boot address
-  //   endcase
-  // end
+  logic [MEM_ADDR_WIDTH-1:0] temp_pc_o; // intermediate pc value
+  logic                      valid_pc_i;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-RTLS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
+  // Instruction Decoder
+  ins_dec #() u_ins_dec (
+    .imem_addr_i(imem_addr_o),    // from PC to IMEM to ID
+    .imem_rdata_i(imem_rdata_i),  // from IMEM
+    .imem_ack_i(imem_ack_i),      // from IMEM
+    .func_o,                      // to reg file
+    .we_o,                        // to reg file
+    .rd_addr_o,                   // to reg file
+    .rs1_addr_o,                  // to reg file
+    .rs2_addr_o,                  // to reg file
+    .imm_o(imm),
+    .valid_pc_o(valid_pc_i)
+  );
+
+  // Register File
+  reg_file #() u_reg_file (
+    
+  )
+
+  // Execution Block
+  merge_execution #() u_merge_execution (
+    .rs1_data_i,  // from reg file
+    .rs2_data_i,  // from reg file
+    .func_i,      // from reg file
+    .imm(imm),    // from instruction decoder
+    .result()     // to reg file
+  )
+
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-METHODS
+  //-ASSIGNMENTS
   //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  always_comb begin
+    case(valid_pc_i)
+      1'b1       :  temp_pc_o   = imem_addr_o + 2; // next pc
+      1'b0       :  temp_pc_o   = boot_addr_i;     // boot address
+      default    :  temp_pc_o   = boot_addr_i;     // for default boot address
+    endcase
+  end
+
+  // hardcode imem_req_o = 1 always
+  assign imem_req_o = 1'b1;
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   //-SEQUENTIALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  // always @(posedge clk_i or negedge arst_ni) begin
-  //   if (~arst_ni) begin
-  //       imm_pc_i <= '0;
-  //   end else begin
-  //       imem_addr_o <= imm_pc_i;
-  //   end
-  // end
+  always @(posedge clk_i or negedge arst_ni) begin
+    if (~arst_ni) begin
+      imem_addr_o <= '0;
+    end else begin
+      imem_addr_o <= temp_pc_o;
+    end
+  end
 
 endmodule
-
