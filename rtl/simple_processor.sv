67476f07f35b889a3de744926df17c887ff4028b
Mymuna Khatun Sadia <maimuna14400@gmail.com>
2024-07-11 21:48:21 +0600

    without dmem

--- rtl/simple_processor.sv
+++ rtl/simple_processor.sv
@@ -3,7 +3,9 @@ Write a markdown documentation for this systemverilog module:
 Author : Mymuna Khatun Sadia (maimuna14400@gmail.com)
 */
 `include "simple_processor_pkg.sv"
-module simple_processor #(
+module simple_processor 
+import simple_processor_pkg::*;
+#(
     parameter int MEM_ADDR_WIDTH = simple_processor_pkg::ADDR_WIDTH,  // Width of memory address bus
     parameter int MEM_DATA_WIDTH = simple_processor_pkg::DATA_WIDTH   // Width of memory data bus
 ) (
